b4cc1a197441b97a108e3d4282fa4a2ba41e47b7
vedang patel <vedangpatel101@gmail.com>
2018-03-23 12:05:01 -0400

    Added the PCA code

--- PCA.py
+++ PCA.py
@@ -1,4 +1,6 @@
-from numpy 					import argsort, cov, column_stack, dot
+from load_compas_data		import load_compas_data
+
+from numpy 					import argsort, average, cov, column_stack, dot
 from numpy.random 			import normal, randn, rand, uniform
 from numpy.linalg			import eig
 from sklearn.decomposition	import PCA
@@ -14,25 +16,26 @@ def generate_data():
 	Y = X_1*2 + X_3*3 + randn(N)
 	return X, Y
 
-X, Y = generate_data()
+# X, Y = generate_data()
+X, Y, x_control = load_compas_data()
 
 N, num_features = X.shape
 sensitive_column = 0				#Define the sensitive column
 
 covar = cov(X, rowvar=False)
 eigenval, eigenvec = eig(covar)
-print eigenvec
 sorttosensattr = argsort(eigenvec[sensitive_column,:])
-print sorttosensattr
+X_norm = X - average(X, axis=0)
 
-for i in range(1, num_features+1):
-	loc_eig = eigenvec[:,sorttosensattr[:i]]
-	X_transform = dot(X,loc_eig)
+# for i in range(1, num_features+1):
+# 	loc_eig = eigenvec[:,sorttosensattr[:i]]
+# 	X_transform = dot(X_norm,loc_eig)
 
 from sklearn.decomposition  import PCA
+
 pca = PCA(n_components=num_features)
 X_trans = pca.fit_transform(X)
-print dot(X, eigenvec)
+print dot(X_norm, eigenvec)
 print X_trans
 import pdb
 pdb.set_trace()
